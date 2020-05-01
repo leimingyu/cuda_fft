3e534bf7e68e375c75d6b21944f9b309f2ef365f
Leiming Yu <leimingyu830@gmail.com>
2020-05-01 15:03:10 -0400

    change datasize to fftsize

--- cufft/tests/test1_planmany.cu
+++ cufft/tests/test1_planmany.cu
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define DATASIZE 8
+#define FFTSIZE 8
 #define BATCH 2
 
 /********************/
@@ -30,14 +30,17 @@ int main (int argc, char* argv[])
 		return -1;
 	};
 
+	int N = FFTSIZE;
+	N = N + 2;
+
 	// --- Host side input data allocation and initialization
-	cufftReal *hostInputData = (cufftReal*)malloc(DATASIZE*BATCH*sizeof(cufftReal));
+	cufftReal *hostInputData = (cufftReal*)malloc(N*BATCH*sizeof(cufftReal));
 	for (int i=0; i<BATCH; i++) {
-		for (int j=0; j<DATASIZE; j++){
-			//hostInputData[i*DATASIZE + j] = (cufftReal)(j + 1);
-			//hostInputData[i*DATASIZE + j] = 100.f; 
-			hostInputData[i*DATASIZE + j] = (float)(j % 10); 
-			printf("%f ", hostInputData[i*DATASIZE + j]);
+		for (int j=0; j<N; j++){
+			//hostInputData[i*FFTSIZE + j] = (cufftReal)(j + 1);
+			//hostInputData[i*FFTSIZE + j] = 100.f; 
+			hostInputData[i*N+ j] = (float)(j % 10); 
+			printf("%f ", hostInputData[i*N + j]);
 		}
 		printf("\n");
 	}
@@ -45,21 +48,21 @@ int main (int argc, char* argv[])
 
 	// --- Device side input data allocation and initialization
 	cufftReal *deviceInputData;
-	gpuErrchk(cudaMalloc((void**)&deviceInputData, DATASIZE * BATCH * sizeof(cufftReal)));
-	cudaMemcpy(deviceInputData, hostInputData, DATASIZE * BATCH * sizeof(cufftReal), cudaMemcpyHostToDevice);
+	gpuErrchk(cudaMalloc((void**)&deviceInputData, N * BATCH * sizeof(cufftReal)));
+	cudaMemcpy(deviceInputData, hostInputData, N * BATCH * sizeof(cufftReal), cudaMemcpyHostToDevice);
 
 	// --- Host side output data allocation
-	cufftComplex *hostOutputData = (cufftComplex*)malloc((DATASIZE / 2 + 1) * BATCH * sizeof(cufftComplex));
+	cufftComplex *hostOutputData = (cufftComplex*)malloc((FFTSIZE / 2 + 1) * BATCH * sizeof(cufftComplex));
 
 	// --- Device side output data allocation
-	cufftComplex *deviceOutputData; gpuErrchk(cudaMalloc((void**)&deviceOutputData, (DATASIZE / 2 + 1) * BATCH * sizeof(cufftComplex)));
+	cufftComplex *deviceOutputData; gpuErrchk(cudaMalloc((void**)&deviceOutputData, (FFTSIZE / 2 + 1) * BATCH * sizeof(cufftComplex)));
 
 	// --- Batched 1D FFTs
 	cufftHandle handle;
 	int rank = 1;                           // --- 1D FFTs
-	int n[] = { DATASIZE };                 // --- Size of the Fourier transform
+	int n[] = { FFTSIZE };                 // --- Size of the Fourier transform
 	int istride = 1, ostride = 1;           // --- Distance between two successive input/output elements
-	int idist = DATASIZE, odist = (DATASIZE / 2 + 1); // --- Distance between batches
+	int idist = FFTSIZE, odist = (FFTSIZE / 2 + 1); // --- Distance between batches
 	int inembed[] = { 0 };                  // --- Input size with pitch (ignored for 1D transforms)
 	int onembed[] = { 0 };                  // --- Output size with pitch (ignored for 1D transforms)
 	int batch = BATCH;                      // --- Number of batched executions
@@ -69,7 +72,7 @@ int main (int argc, char* argv[])
 
 	if(plantype == 1) {
 		printf("cufftplan1d\n");
-		cufftPlan1d(&handle, DATASIZE, CUFFT_R2C, BATCH);
+		cufftPlan1d(&handle, FFTSIZE, CUFFT_R2C, BATCH);
 	}
 
 	if(plantype == 2) {
@@ -83,11 +86,11 @@ int main (int argc, char* argv[])
 	cufftExecR2C(handle,  deviceInputData, deviceOutputData);
 
 	// --- Device->Host copy of the results
-	gpuErrchk(cudaMemcpy(hostOutputData, deviceOutputData, (DATASIZE / 2 + 1) * BATCH * sizeof(cufftComplex), cudaMemcpyDeviceToHost));
+	gpuErrchk(cudaMemcpy(hostOutputData, deviceOutputData, (FFTSIZE / 2 + 1) * BATCH * sizeof(cufftComplex), cudaMemcpyDeviceToHost));
 
 	for (int i=0; i<BATCH; i++)
-		for (int j=0; j<(DATASIZE / 2 + 1); j++)
-			printf("%i %i %f %f\n", i, j, hostOutputData[i*(DATASIZE / 2 + 1) + j].x, hostOutputData[i*(DATASIZE / 2 + 1) + j].y);
+		for (int j=0; j<(FFTSIZE / 2 + 1); j++)
+			printf("%i %i %f %f\n", i, j, hostOutputData[i*(FFTSIZE / 2 + 1) + j].x, hostOutputData[i*(FFTSIZE / 2 + 1) + j].y);
 
 	cufftDestroy(handle);
 	gpuErrchk(cudaFree(deviceOutputData));
